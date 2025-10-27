68889551738baf4144140b291f08ca32af558c0c
chris <christiiinereyes@gmail.com>
2025-10-27 20:17:13 +0800

    added screens, basic html format for about us and home page

---
+++ src/Screens/About_us.jsx
@@ -0,0 +1,51 @@
+function About_us() {
+    return (
+        <>
+         <section className="navbar">
+            <div>
+                <img></img>
+            </div>
+            <div>
+                Home About Us Services
+            </div>
+            <div>
+                Book Appointment
+            </div>
+        </section>
+        
+        <section id="aboutushero">
+            <div>
+                <h2> About Menchie's Dental Clinic </h2>
+                <p> lorem ipsum text </p>
+            </div>
+
+            <section>
+                <div>
+                    <img></img>
+                    <h1> Stress-Free </h1>
+                    <p> lorem ipsum text </p>
+                </div>
+                <div>
+                    <img></img>
+                    <h1> Affordable </h1>
+                    <p> lorem ipsum text </p>
+                </div>
+                <div>
+                    <img></img>
+                    <h1> Personalized </h1>
+                    <p> lorem ipsum text </p>
+                </div>
+            </section>
+        </section>
+
+        <hr></hr>
+
+        
+    
+     </>
+       
+
+    );
+}
+
+export default About_us;
---
+++ src/Screens/Home.jsx
@@ -0,0 +1,36 @@
+function Home() {
+    return (
+        <>
+         <section className="navbar">
+            <div>
+                <img></img>
+            </div>
+            <div>
+                Home About Us Services
+            </div>
+            <div>
+                Book Appointment
+            </div>
+        </section>
+        
+        <section id="homehero">
+            <div>
+                <h1> Modern Dentistry for a Perfect Smile </h1>
+                <h3> lorem lorem lorem blah blah blah </h3>
+                <p> vlah vlah more descriptions</p>
+
+                <button> Working Hours </button> <button> Contact Us </button>
+            </div>
+        </section>
+        <section id="imagecarousel">
+            <img></img>
+            <div id="dots">. . .</div>
+        </section>
+
+     </>
+       
+
+    );
+}
+
+export default Home;
